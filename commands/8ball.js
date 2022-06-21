e44215e4496e3e1a09b535bfb36cc3c9bd3f2b64
EMG_DEV <nabeellatheef9@gmail.com>
2022-06-21 22:11:00 +0530

    version 0.0.1 release success

--- commands/8ball.js
+++ commands/8ball.js
@@ -13,7 +13,7 @@ const { guild, channel } = message
 
                         //PERMISSION CHECKS
 if (!message.channel.permissionsFor(message.client.user).has("SEND_MESSAGES")) return;
-if (!message.channel.permissionsFor(message.client.user).has("EMBED_LINKS")) return message.channel.send(`:x: Please Enable **EMBED_LINKS** Pemission For Me`);
+if (!message.channel.permissionsFor(message.client.user).has("EMBED_LINKS")) return message.channel.send(` Please Enable **EMBED_LINKS** Pemission For Me`);
 
 const roast = ([`Yes :white_check_mark:`,
                   `No :x:`,
@@ -26,7 +26,7 @@ const result = Math.floor((Math.random() * roast.length));
 
 const dumb = new discord.MessageEmbed()
 .setColor([227, 114, 237])
-.setDescription(`${p}8ball [QUESTION] \n \neg: \n||${p}8ball am i a good bot?||`)
+.setDescription(`${p}8ball [QUESTION] \n \neg: ||${p}8ball am i a good bot?||`)
 
 const ball = new discord.MessageEmbed()
 .setColor([227, 114, 237])
@@ -34,6 +34,7 @@ const ball = new discord.MessageEmbed()
 
 
   if(!args[1]) return message.channel.send({embeds: [dumb]});
+  if(!message.content.endsWith("?")) return message.channel.send("Are you sure that it's a question?")
 
 
   message.channel.send({embeds: [ball]})
